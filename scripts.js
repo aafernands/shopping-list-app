ee0c89afedec8331dacb33865e3861916f86b16b
Alex Fernandes <alexfernands@outlook.com>
2024-12-29 18:08:00 -0500

    fixed bugs

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
-const savePerson = () => {
-  const item = document.getElementById("name").value;
+const saveItem = () => {
+  const item = document.getElementById("item").value;
   const qty = document.getElementById("qty").value;
 
-  const items = { item, age };
+  const items = { item, qty };
 
   localStorage.setItem("items", JSON.stringify(items));
 
@@ -13,18 +13,18 @@ const savePerson = () => {
 
 const displayItems = () => {
   const output = document.getElementById("output");
-  const person = localStorage.getItem("items");
+  const items = localStorage.getItem("items");
 
   if (items) {
     const parsedItems = JSON.parse(items);
     output.innerHTML = `<strong> Stored Items: </strong> <br>
-                          Name: ${parsedItems.item} <br>
-                          Age: ${parsedItems.qty}`;
+                          Item: ${parsedItems.item} <br>
+                          Quantity: ${parsedItems.qty}`;
   } else {
     output.innerHTML = `<strong> No items data found in localStorage.: </strong>`;
   }
 };
 
-// const deletePerson = () => {
-//   alert("Deleted");
-// };
+const deleteItem = () => {
+  alert("Item Deleted");
+};
